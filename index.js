57ce645ef77263d558e539648f3377e33aa86e7b
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-04 20:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser}.slice(0, 1)!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
