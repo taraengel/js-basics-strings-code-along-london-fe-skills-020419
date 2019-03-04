0da7daf3be6ee44e4c00472801da8cbab939e2aa
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-04 20:31:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Tara Engelhardt';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
